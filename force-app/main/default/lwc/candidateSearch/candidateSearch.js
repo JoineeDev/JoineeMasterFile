905a4a8e9ef865b97901503ff9fd8c0491772a6d
Shivam Ojha <shivam_ojha@persistent.com>
2021-02-25 23:51:44 +0530

    Candidate Form Changes

--- force-app/main/default/lwc/candidateSearch/candidateSearch.js
+++ force-app/main/default/lwc/candidateSearch/candidateSearch.js
@@ -3,6 +3,7 @@ import { LightningElement, track } from 'lwc';
 export default class CandidateSearch extends LightningElement {
     @track searchResultAvailable = false;
     handleSearch(event) {
+        //Added Comment for Git
         this.searchResultAvailable = true;
         alert(this.searchResultAvailable);
     }
